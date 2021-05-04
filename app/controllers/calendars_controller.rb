a155f2ec1e343b9ec6255a7e12d966f496cb956f
tsuyoshitorikawa <gangtulichuan@gmail.com>
2021-05-04 17:00:21 +0900

    Revert "issue3"
    This reverts commit 62115d9da88fcdb133127206b442f86d8bfcf378.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -9,13 +9,13 @@ class CalendarsController < ApplicationController
   # 予定の保存
   def create
     Plan.create(plan_params)
-    redirect_to action: :index
+    Redirect_to action: :index
   end
 
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    Params.require(:calendars).permit(:date, :plan)
   end
 
   def getWeek
